049ee4d69c27f5f9c8a20f36a18d0cb8a31455d2
Mathias Wriedt Kamp <math260d@zbc.dk>
2020-11-09 16:39:49 +0100

    Add project files.

---
+++ CoffeeMachine/CoffeeMachine.cs
@@ -0,0 +1,43 @@
+﻿using System.Security.Cryptography;
+
+namespace CoffeeMachine
+{
+    public  class CoffeeMachine :HotDrinkMaker, IChooseProduct
+    {
+        public CoffeeMachine(double waterContainer, double productContainer, int numberOfCups) : base(waterContainer, productContainer, numberOfCups)
+        {
+
+        }
+
+        public string[] Products()
+        {
+            string [] product = new string[]{"Whole Beans", "Ground Beans", "Capsules"};
+
+            return product;
+        }
+
+        public override double FillWaterContainer()
+        {
+            return waterContainer = 1;
+        }
+
+
+        public override double FillProductContainer()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override string GetNumberOfCups()
+        {
+            double fillWaterContainer = FillWaterContainer();
+            string numberOfCups = null;
+
+            if (fillWaterContainer == 1)
+            {
+                numberOfCups = "Number of cups to be made is 5";
+            }
+
+            return numberOfCups;
+        }
+    }
+}
---
+++ CoffeeMachine/HotDrinkMaker.cs
@@ -0,0 +1,52 @@
+﻿namespace CoffeeMachine
+{
+    public abstract class HotDrinkMaker
+    {
+        protected double waterContainer;
+
+        public double WaterContainer
+        {
+            get { return waterContainer; }
+            set { waterContainer = value; }
+        }
+
+        protected double productContainer;
+
+        public double ProductContainer
+        {
+            get { return productContainer; }
+            set { productContainer = value; }
+        }
+
+        protected int numberOfCups;
+
+        public int NumberOfCups
+        {
+            get { return numberOfCups; }
+            set { numberOfCups = value; }
+        }
+
+        protected bool startButton;
+
+        public bool StartButton
+        {
+            get { return startButton; }
+            set { startButton = value; }
+        }
+
+        public abstract double FillWaterContainer();
+
+        public abstract double FillProductContainer();
+
+        public abstract string GetNumberOfCups();
+
+
+        protected HotDrinkMaker(double waterContainer, double productContainer, int numberOfCups)
+        {
+            this.numberOfCups = numberOfCups;
+            this.productContainer = productContainer;
+            this.waterContainer = waterContainer;
+        }
+
+    }
+}
